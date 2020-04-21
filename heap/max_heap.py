2c0247581767d27fd4e83b18db2c4e86e666e6d0
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-21 09:01:51 -0700

    completes insert and _bubble_up methods

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,7 +3,9 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        if len(self.storage) > 1:
+            self._bubble_up(len(self.storage)-1)
 
     def delete(self):
         pass
@@ -15,7 +17,27 @@ class Heap:
         pass
 
     def _bubble_up(self, index):
-        pass
+        if index % 2 == 0:
+            parent_index = (index-2)//2
+        else:
+            parent_index = (index-1)//2
+        if self.storage[index] <= self.storage[parent_index]:
+            return
+        self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
+        if parent_index > 0:
+            self._bubble_up(parent_index)
 
     def _sift_down(self, index):
         pass
+
+
+heap = Heap()
+heap.insert(3)
+heap.insert(25)
+heap.insert(19)
+heap.insert(17)
+heap.insert(16)
+heap.insert(40)
+heap.insert(24)
+heap.insert(30)
+print(heap.storage)
